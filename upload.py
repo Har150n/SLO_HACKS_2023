7b0c209c98f3e2c02c05738cf24b5dbbc539c95d
Harrison Shu <hshu02@calpoly.edu>
2023-05-13 13:58:20 -0700

    skeleton game class

--- upload.py
+++ upload.py
@@ -1,18 +1,20 @@
 import tkinter as tk
 from tkinter import filedialog
 
-# Create a Tkinter window to allow file selection
-root = tk.Tk()
-root.withdraw()
+def prompt_upload():
+    # Create a Tkinter window to allow file selection
+    root = tk.Tk()
+    root.withdraw()
 
-# Ask the user to select multiple image files
-print("Please select the images to process:")
-file_paths = filedialog.askopenfilenames()
+    # Ask the user to select multiple image files
+    print("Please select the images to process:")
+    file_paths = filedialog.askopenfilenames()
 
-# Process each selected image
-for file_path in file_paths:
-    # Read the image using OpenCV
-    print(file_path)
+    # Process each selected image
+    for file_path in file_paths:
+        # Read the image using OpenCV
+        print(file_path)
 
-# Close the Tkinter window
-root.destroy()
+    # Close the Tkinter window
+    root.destroy()
+    return file_paths
