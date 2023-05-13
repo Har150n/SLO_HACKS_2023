bd4da0068de7d751b8dc639d1111b67dde60617d
Parshana007 <parshana007@gmail.com>
2023-05-13 15:05:32 -0700

    added nav bar

--- functions.py
+++ functions.py
@@ -1,34 +1,71 @@
 import tkinter as tk
 
-def create_game_window(root_home):
-    # Hide the current window
-    root_home.withdraw()
-    
+def create_game_window(root_home, windows):
     # Create a new window
     game_window = tk.Toplevel(root_home)
+
+    # Add to other_windows
+    windows["game_window"] = game_window
+
+    # Hide the current window
+    for window_name, window in windows.items():
+        if window_name != "game_window":
+            window.withdraw()
+
     game_window.title("EMO Quest -- Game")
     game_window.geometry("1000x900")
-    
+
+    # Create a frame for the navigation bar
+    navbar_frame = tk.Frame(game_window, bg="lightblue", height=30)
+    navbar_frame.pack(side=tk.TOP, fill=tk.X, anchor=tk.NE)
+
     # Add a back arrow button to the new window
-    back_button = tk.Button(game_window, text="Home", command=lambda: [game_window.destroy(), root_home.deiconify()])
-    back_button.pack(pady=10, side=tk.TOP,anchor=tk.NW)
+    back_button = tk.Button(navbar_frame, text="Home", bg="blue", font=("Times", 20), command=lambda: [game_window.destroy(), root_home.deiconify()])
+    back_button.pack(side=tk.LEFT)
+
+    # Add a upload photos button
+    button_photos = tk.Button(navbar_frame, text="Upload Photos", font=("Times", 20), command=lambda:create_photos_window(root_home, windows))
+    button_photos.pack(side=tk.RIGHT)
+
+    # Add a play Game button
+    button_game = tk.Button(navbar_frame, text="Play Game", font=("Times", 20), command=lambda:create_game_window(root_home, windows))
+    button_game.pack(side=tk.RIGHT)
+
     
     # Add widgets to the new window
     label = tk.Label(game_window, text="This is the new window!")
     label.pack(pady=10)
 
-def create_photos_window(root_home):
-    # Hide the current window
-    root_home.withdraw()
-    
+def create_photos_window(root_home, windows):
     # Create a new window
     photos_window = tk.Toplevel(root_home)
+
+    # Add to other_windows
+    windows["photos_window"] = photos_window
+
+    # Hide the current window
+    for window_name, window in windows.items():
+        if window_name != "photos_window":
+            window.withdraw()
+
     photos_window.title("EMO Quest -- Photos")
     photos_window.geometry("1000x900")
-    
+
+    # Create a frame for the navigation bar
+    navbar_frame = tk.Frame(photos_window, bg="lightblue", height=30)
+    navbar_frame.pack(side=tk.TOP, fill=tk.X, anchor=tk.NE)
+
     # Add a back arrow button to the new window
-    back_button = tk.Button(photos_window, text="Home", command=lambda: [photos_window.destroy(), root_home.deiconify()])
-    back_button.pack(pady=10, side=tk.TOP,anchor=tk.NW)
+    back_button = tk.Button(navbar_frame, text="Home", bg="blue", font=("Times", 20), command=lambda: [game_window.destroy(), root_home.deiconify()])
+    back_button.pack(side=tk.LEFT)
+
+    # Add a upload photos button
+    button_photos = tk.Button(navbar_frame, text="Upload Photos", font=("Times", 20), command=lambda:create_photos_window(root_home, windows))
+    button_photos.pack(side=tk.RIGHT)
+
+    # Add a play Game button
+    button_game = tk.Button(navbar_frame, text="Play Game", font=("Times", 20), command=lambda:create_game_window(root_home, windows))
+    button_game.pack(side=tk.RIGHT)
     
     # Add widgets to the new window
     label = tk.Label(photos_window, text="This is the new photo window!")
--- home.py
+++ home.py
@@ -1,27 +1,37 @@
 import tkinter as tk
 from functions import create_game_window
 from functions import create_photos_window
-#from deepface import DeepFace
+
+windows = {}
 
 # Create home path
 root_home = tk.Tk()
 root_home.title("EMO Quest")
 root_home.geometry("1000x900")
+windows["root_home"] = root_home
+
+# Create a frame for the navigation bar
+navbar_frame = tk.Frame(root_home, bg="lightblue", height=30)
+navbar_frame.pack(side=tk.TOP, fill=tk.X, anchor=tk.NE)
+
+# Add a home button
+button_home = tk.Button(navbar_frame, text="Home", bg="blue", font=("Times", 20))
+button_home.pack(side=tk.LEFT)
+
+# Add a upload photos button
+button_photos = tk.Button(navbar_frame, text="Upload Photos", bg="darkblue", font=("Times", 20), command=lambda:create_photos_window(root_home, windows))
+button_photos.pack(side=tk.RIGHT)
+
+# Add a play Game button
+button_game = tk.Button(navbar_frame, text="Play Game", bg="darkblue", font=("Times", 20), command=lambda:create_game_window(root_home, windows))
+button_game.pack(side=tk.RIGHT)
 
 # Hold contents of home page
 main_frame = tk.Frame(root_home)
 main_frame.pack(fill=tk.BOTH, expand=True)
 
 # Add a label for the heading
-heading_label = tk.Label(main_frame, text="Welcome to EMO Quest", font=("Arial", 24))
+heading_label = tk.Label(main_frame, text="Welcome to EMO Quest", font=("Times", 60))
 heading_label.pack()
-
-# Add a upload photos button
-button_photos = tk.Button(main_frame, text="Upload Photos", command=lambda:create_photos_window(root_home))
-button_photos.pack()
-
-# Add a play Game button
-button_game = tk.Button(main_frame, text="Play Game", command=lambda:create_game_window(root_home))
-button_game.pack()
 
 root_home.mainloop()
