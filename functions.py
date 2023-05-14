b82f64f8f06f017eaa05d2664b9d5913a3f3089d
Parshana007 <parshana007@gmail.com>
2023-05-13 18:04:26 -0700

    got rid of create_photos_window

--- functions.py
+++ functions.py
@@ -33,78 +33,15 @@ def create_game_window(root_home, windows):
     button_game = tk.Button(navbar_frame, text="Play Game", font=("Times", 20))
     button_game.pack(side=tk.RIGHT)
 
-    # Add a upload photos button
-    button_photos = tk.Button(navbar_frame, text="Upload Photos", font=("Times", 20), command=lambda:create_photos_window(root_home, windows))
-    button_photos.pack(side=tk.RIGHT)
-
     # Add a back arrow button to the new window
     back_button = tk.Button(navbar_frame, text="Home", bg="blue", font=("Times", 20), command=lambda: [game_window.destroy(), root_home.deiconify()])
     back_button.pack(side=tk.RIGHT)
     
     # Add widgets to the new window
-    label = tk.Label(game_window, text="This is the new window!")
+    label = tk.Label(game_window, text="Starting the game")
     label.pack(pady=10)
 
     # Add Logo
     game_window.logo_image = tk.PhotoImage(file="EMO_logo.png").subsample(2)
     label = tk.Label(game_window, image=game_window.logo_image)
-    label.place(relx=1.0, rely=1.0, anchor=tk.SE)
-
-
-def create_photos_window(root_home, windows):
-    # Create a new window
-    photos_window = tk.Toplevel(root_home)
-
-    # Add to other_windows
-    windows["photos_window"] = photos_window
-
-    # Hide the current window
-    for window_name, window in windows.items():
-        if window_name != "photos_window" and window.winfo_exists():
-            window.withdraw()
-
-    screen_width = photos_window.winfo_screenwidth()
-    screen_height = photos_window.winfo_screenheight()
-
-    # Set the window size and position
-    window_width = int(screen_width)
-    window_height = int(screen_height)
-    window_x = int((screen_width - window_width) / 2)
-    window_y = int((screen_height - window_height) / 2)
-
-    photos_window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
-
-    photos_window.title("EMO Quest -- Photos")
-
-    # Create a frame for the navigation bar
-    navbar_frame = tk.Frame(photos_window, bg="lightblue", height=30)
-    navbar_frame.pack(side=tk.TOP, fill=tk.X, anchor=tk.NE)
-
-    # Add a play Game button
-    button_game = tk.Button(navbar_frame, text="Play Game", font=("Times", 20), command=lambda:create_game_window(root_home, windows))
-    button_game.pack(side=tk.RIGHT)
-
-    # Add a upload photos button
-    button_photos = tk.Button(navbar_frame, text="Upload Photos", font=("Times", 20))
-    button_photos.pack(side=tk.RIGHT)
-
-    # Add a back arrow button to the new window
-    back_button = tk.Button(navbar_frame, text="Home", bg="blue", font=("Times", 20), command=lambda: [photos_window.destroy(), root_home.deiconify()])
-    back_button.pack(side=tk.RIGHT)
-
-    # Add empty label for padding
-    padding_label = tk.Label(photos_window, height=8)
-    padding_label.pack()
-
-    # Add a label for the heading
-    heading_label = tk.Label(photos_window, text="Upload A Collection of Pictures", font=("Helvetica", 60))
-    heading_label.pack()
-
-    # Add a play Game button
-    button_addPhotos = tk.Button(photos_window, text="Click here to upload", font=("Times", 20))
-    button_addPhotos.pack()
-
-    # Add Logo
-    photos_window.logo_image = tk.PhotoImage(file="EMO_logo.png").subsample(2)
-    label = tk.Label(photos_window, image=photos_window.logo_image)
     label.place(relx=1.0, rely=1.0, anchor=tk.SE)
--- home.py
+++ home.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from functions import create_game_window
-from functions import create_photos_window
 
 def create_homepage():
     windows = {}
@@ -26,76 +25,45 @@ def create_homepage():
     navbar_frame = tk.Frame(root_home, bg="lightblue", height=30)
     navbar_frame.pack(side=tk.TOP, fill=tk.X, anchor=tk.NE)
 
-<<<<<<< Updated upstream
+    # Add a play Game button
+    button_game = tk.Button(navbar_frame, text="Play Game", bg="blue", font=("Times", 20), command=lambda:create_game_window(root_home, windows))
+    button_game.pack(side=tk.RIGHT)
+
     # Add a home button
     button_home = tk.Button(navbar_frame, text="Home", bg="blue", font=("Times", 20))
-    button_home.pack(side=tk.LEFT)
-
-    # Add a upload photos button
-    button_photos = tk.Button(navbar_frame, text="Upload Photos", bg="darkblue", font=("Times", 20), command=lambda:create_photos_window(root_home, windows))
-    button_photos.pack(side=tk.RIGHT)
-
-    # Add a play Game button
-    button_game = tk.Button(navbar_frame, text="Play Game", bg="darkblue", font=("Times", 20), command=lambda:create_game_window(root_home, windows))
-    button_game.pack(side=tk.RIGHT)
-=======
-# Add a play Game button
-button_game = tk.Button(navbar_frame, text="Play Game", bg="blue", font=("Times", 20), command=lambda:create_game_window(root_home, windows))
-button_game.pack(side=tk.RIGHT)
-
-# Add a upload photos button
-button_photos = tk.Button(navbar_frame, text="Upload Photos", bg="blue", font=("Times", 20), command=lambda:create_photos_window(root_home, windows))
-button_photos.pack(side=tk.RIGHT)
-
-# Add a home button
-button_home = tk.Button(navbar_frame, text="Home", bg="blue", font=("Times", 20))
-button_home.pack(side=tk.RIGHT)
->>>>>>> Stashed changes
+    button_home.pack(side=tk.RIGHT)
 
     # Hold contents of home page
     main_frame = tk.Frame(root_home)
     main_frame.pack(fill=tk.BOTH, expand=True)
 
-<<<<<<< Updated upstream
     # Add empty label for padding
-    padding_label = tk.Label(main_frame, height=10)
+    padding_label = tk.Label(main_frame, height=8)
     padding_label.pack()
-=======
-# Add empty label for padding
-padding_label = tk.Label(main_frame, height=8)
-padding_label.pack()
->>>>>>> Stashed changes
 
     # Add a label for the heading
     heading_label = tk.Label(main_frame, text="Welcome to EMO Quest", font=("Helvetica", 60))
     heading_label.pack()
 
-<<<<<<< Updated upstream
+    # Add about section
+    subheading_label = tk.Label(main_frame, 
+        text = """
+                Welcome to our game, designed with a heartfelt mission to help autistic 
+                individuals better understand and recognize the emotions of those around them. 
+                Our game is specifically tailored to improve facial recognition of emotions 
+                from close family and friends, providing a safe and engaging environment for 
+                self-improvement. We understand the unique challenges faced by individuals on 
+                the autism spectrum, and our team is dedicated to creating a game that is both 
+                enjoyable and beneficial for their personal growth. With our game, we hope to 
+                provide a platform for individuals to enhance their social and emotional 
+                understanding, ultimately leading to a more fulfilling and enriched life.""", 
+                font=("Arial", 16))
+
+    subheading_label.pack()
+
     # Add Logo
     image = tk.PhotoImage(file="EMO_logo.png")
     label = tk.Label(root_home, image=image)
     label.pack()
-=======
-# Add about section
-subheading_label = tk.Label(main_frame, 
-    text = """
-            Welcome to our game, designed with a heartfelt mission to help autistic 
-            individuals better understand and recognize the emotions of those around them. 
-            Our game is specifically tailored to improve facial recognition of emotions 
-            from close family and friends, providing a safe and engaging environment for 
-            self-improvement. We understand the unique challenges faced by individuals on 
-            the autism spectrum, and our team is dedicated to creating a game that is both 
-            enjoyable and beneficial for their personal growth. With our game, we hope to 
-            provide a platform for individuals to enhance their social and emotional 
-            understanding, ultimately leading to a more fulfilling and enriched life.""", 
-            font=("Arial", 16))
-
-subheading_label.pack()
-
-# Add Logo
-image = tk.PhotoImage(file="EMO_logo.png")
-label = tk.Label(root_home, image=image)
-label.pack()
->>>>>>> Stashed changes
 
     root_home.mainloop()
